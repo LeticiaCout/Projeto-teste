ea1222b9d1bc88f77bb522761a8f14ab78a61dc1
Letícia Coutinho <leticiacout24@gmail.com>
2024-11-08 19:34:39 -0300

    Iniciando projeto

---
+++ mysite/polls/views.py
@@ -0,0 +1,9 @@
+from http.client import HTTPResponse
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("Hello, word. You're at the polls index.")
