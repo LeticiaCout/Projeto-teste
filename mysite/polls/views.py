14c071bffcc46fe3c451a2ea0d158326268b2246
Sala 304B <sala304b@ceines.com.br>
2024-11-08 20:06:25 -0300

    Alterando a view de polls

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -6,4 +6,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, word. You're at the polls index.")
+    return HttpResponse("Olá, meu nome não é Johnny")
